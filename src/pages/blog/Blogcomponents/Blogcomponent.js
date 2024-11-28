173e4c40b902a7928deec2c36d6afd792dcac5c8
anil <anilmekala54@gmail.com>
2024-11-28 13:07:41 +0530

    no change

--- src/pages/blog/Blogcomponents/Blogcomponent.js
+++ src/pages/blog/Blogcomponents/Blogcomponent.js
@@ -5,7 +5,7 @@ export default function Blogcomponent() {
   return (
       <div className="shop-details">
           <div className="shop-details-content">
-            <h1>Shop Details</h1>
+            <h1>Blog List</h1>
             <h2>Home &gt;<span className='shop-details-text'>Blog</span></h2>
           </div>
       </div>
