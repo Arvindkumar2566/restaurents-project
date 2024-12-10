65899e1f2a345bc03abc36d8c77a1543215365a9
anil <anilmekala54@gmail.com>
2024-12-10 17:49:05 +0530

    complede functionality of search

--- src/pages/searchfooditems/searchcomponents/Searchfoodbody.js
+++ src/pages/searchfooditems/searchcomponents/Searchfoodbody.js
@@ -1,31 +1,24 @@
-import React from 'react'
-import './Searchfoodbody.css'
+import React from 'react';
+import './Searchfoodbody.css';
 
-function Searchfoodbody() {
+function Searchfoodbody({ data }) {
   return (
-    <div>
-         <div className="row" >
-      <div className='col-md-4'> 
-      <div className="card" style={{ width: '18rem' }}>
-      <img
-        src="https://via.placeholder.com/150"
-        className="card-img-top"
-        alt="Card example"
-      />
-      <div className="card-body">
-        <h5 className="card-title">Card Title</h5>
-        <p className="card-text">
-          This is a simple card example using Bootstrap in React.
-        </p>
-        <a href="#" className="btn btn-primary">
-          Go somewhere
-        </a>
-      </div>
-      </div>
+    <div className="food-cards">
+      {data?.map((meal, index) => (
+        <div key={index} className>
+          <div className="card mt-4" style={{ width: '18rem' }}>
+            <img src={meal.strMealThumb} className="card-img-top" alt={meal.strMeal} />
+            <div className="card-body">
+              <h5 className="card-title">{meal.strMeal}</h5>
+              <a href={meal.strYoutube} className="btn  view-btn" target="_blank" rel="noopener noreferrer">
+                View Recipe
+              </a>
+            </div>
+          </div>
+        </div>
+      ))}
     </div>
-      </div>
-    </div>
-  )
+  );
 }
 
-export default Searchfoodbody
+export default Searchfoodbody;
