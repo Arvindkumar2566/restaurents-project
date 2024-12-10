f90dc2b37311b01fcd79edded0ff451ab4ec52b0
anil <anilmekala54@gmail.com>
2024-12-10 16:16:13 +0530

    added

--- src/pages/searchfooditems/searchcomponents/Searchfoodbody.js
+++ src/pages/searchfooditems/searchcomponents/Searchfoodbody.js
@@ -4,7 +4,26 @@ import './Searchfoodbody.css'
 function Searchfoodbody() {
   return (
     <div>
-      
+         <div className="row" >
+      <div className='col-md-4'> 
+      <div className="card" style={{ width: '18rem' }}>
+      <img
+        src="https://via.placeholder.com/150"
+        className="card-img-top"
+        alt="Card example"
+      />
+      <div className="card-body">
+        <h5 className="card-title">Card Title</h5>
+        <p className="card-text">
+          This is a simple card example using Bootstrap in React.
+        </p>
+        <a href="#" className="btn btn-primary">
+          Go somewhere
+        </a>
+      </div>
+      </div>
+    </div>
+      </div>
     </div>
   )
 }
